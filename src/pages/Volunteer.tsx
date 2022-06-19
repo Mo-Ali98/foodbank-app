75cdf034cc47765205b1a53d7ed805a6d7bd2530
Nuh Mire <miren321@hotmail.co.uk>
2022-06-19 13:32:55 +0100

    Page Cleanup

--- src/pages/Volunteer.tsx
+++ src/pages/Volunteer.tsx
@@ -1,12 +1,18 @@
 import React, {useState} from "react";
 import styled from "styled-components";
 
+interface IBtn {
+    isClicked: string[],
+    assignedTo: string,
+  }
+
 const Container = styled.div`
     display: flex;
     flex-direction: column;
     padding-top:5%;
     padding-left:20%;
     padding-right:20%;
+    // flex-wrap: wrap;
 `;
 
 const Title = styled.div`
@@ -40,15 +46,11 @@ const ContactTitle = styled.div`
     font-size: 24px;
 `;
 
-const SubmitText = styled.div`
+const DivPadding = styled.div`
     padding-top:2%;
 `;
 
-const LowerSection = styled.div`
-    padding-top:2%;
-`;
-
-const SubmitButton = styled.div`
+const SubmitSection = styled.div`
     display: flex;
     flex-direction: column;
     margin-left: auto;
@@ -60,12 +62,7 @@ const SubmitButton = styled.div`
 `;
 
 const CardContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    margin:2%;
     padding-top:10%;
-    padding-left:20%;
-    padding-right:20%;
 `;
 
 const CardTitle = styled.div`
@@ -82,6 +79,26 @@ const CardBody = styled.div`
     font-size: 16px;
 `;
 
+const Scroll = styled.div`
+    overflow-x: scroll;
+    overflow-y: hidden;
+    white-space: nowrap;
+`;
+const Card = styled.div`
+    display: inline-block;
+    padding-top:5%;
+    width: 10rem;
+    margin:1%;
+`;
+const CardButton = styled.button<IBtn>`
+    background-color: ${props => props.isClicked.includes(props.assignedTo) ? '#7D57C2' : 'white'}; 
+    color: ${props => props.isClicked.includes(props.assignedTo) ? 'white' : 'black'}; ; 
+    border: 2px solid #008CBA;
+    border-radius: 5px;
+    ${CardBody}:hover & {
+        background-color: '#7D57C2';
+      }  
+`;
 
 export const Volunteer = () => {
     const [firstName, setFirstName] = useState<string>("");
@@ -93,8 +110,7 @@ export const Volunteer = () => {
     const [dateSelected, setDateSelected] = useState<string[]>([]);
 
 
-    
-    const dates: string[]= ["21-06-2021","23-06-2022","24-06-2022","27-06-2022","28-06-2022"];
+    const dates: string[]= ["21-06-2021","23-06-2022","24-06-2022","27-06-2022","28-06-2022","29-06-2021","13-07-2022","14-08-2022","17-09-2022","28-09-2022"];
 
     const getMonth = (val:string) :string => {
         const months: string[]= ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
@@ -115,7 +131,7 @@ export const Volunteer = () => {
 
     return(
         <Container>
-            <Title>Event Name</Title>
+            <Title>Organisation Name</Title>
             <BannerSection>
                 <EventText>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
@@ -134,21 +150,26 @@ export const Volunteer = () => {
                     <img src="https://www.w3schools.com/css/img_5terre_wide.jpg" alt="Paris" width= "450px" height= "250px"/>
                 </Image>
             </BannerSection>
-            <CardContainer>
+            <CardContainer> 
+            <Title>Please scroll horizontally and select dates you are available</Title>
+            <Scroll>
                 {dates.map((item: string,key:number) => (
-                        <div className="card" style={{width: "20rem",margin:"1%",flexWrap: "wrap"}} key = {key}>
+                        <Card key={key}>
                             <div className="card-body">
                                 <CardTitle>{item.split("-")[0]}</CardTitle>
                                 <CardBody>{getMonth(item.split("-")[1].split("0")[1])}</CardBody>
                                 <CardBody>{item.split("-")[2]}</CardBody>
-                                <CardBody><button type="button" className="btn btn-outline-primary btn-sm" 
-                                onClick={(e => {
-                                    setDateSelected((prev) => [...prev,item]);
-                                    console.log(dateSelected);
-                                })}>Select</button></CardBody>
+                                <CardBody>
+                                    <CardButton isClicked = {dateSelected} assignedTo={item} onClick={(e => {
+                                        setDateSelected((prev) => [...prev,item]);
+                                        })}>
+                                        Select
+                                    </CardButton>
+                                </CardBody>
                             </div>
-                        </div>
+                        </Card>
                 ))}
+                </Scroll>
             </CardContainer>   
             <ContactContainer>
                 <ContactTitle>Billing address</ContactTitle>
@@ -157,7 +178,7 @@ export const Volunteer = () => {
                     <div className="col-md-6 mb-3">
                         <label htmlFor="firstName">First name</label>
                         <input type="text" className="form-control" id="firstName" placeholder="" required 
-                            onChange={(e) => setFirstName(e.target.value)}/>
+                            onChange={(e) => setFirstName(e.target.value)} style={{borderColor:"#7D57C2"}}/>
                         <div className="invalid-feedback">
                         Valid first name is required.
                         </div>
@@ -165,7 +186,7 @@ export const Volunteer = () => {
                     <div className="col-md-6 mb-3">
                         <label htmlFor="lastName">Last name</label>
                         <input type="text" className="form-control" id="lastName" placeholder="" required
-                            onChange={(e) => setLastName(e.target.value)}/>
+                            onChange={(e) => setLastName(e.target.value)} style={{borderColor:"#7D57C2"}}/>
                         <div className="invalid-feedback">
                         Valid last name is required.
                         </div>
@@ -175,7 +196,7 @@ export const Volunteer = () => {
                     <div className="col-md-6 mb-3">
                         <label htmlFor="Email Address">Email Address</label>
                         <input type="text" className="form-control" id="email" placeholder=""  required
-                            onChange={(e) => setEmail(e.target.value)}/>
+                            onChange={(e) => setEmail(e.target.value)} style={{borderColor:"#7D57C2"}}/>
                         <div className="invalid-feedback">
                         Valid emaill address is required.
                         </div>
@@ -183,24 +204,24 @@ export const Volunteer = () => {
                     <div className="col-md-6 mb-3">
                         <label htmlFor="Mobile Number">Mobile Number</label>
                         <input type="text" className="form-control" id="mobileNumber" placeholder=""  required
-                            onChange={(e) => setMobile(e.target.value)}/>
+                            onChange={(e) => setMobile(e.target.value)} style={{borderColor:"#7D57C2"}}/>
                         <div className="invalid-feedback">
                         Valid mobile number is required.
                         </div>
                     </div>
-                    <LowerSection>
+                    <DivPadding>
                         <h6 className="mb-3">Question 1 - This is a Dummy question, enter your answer below</h6>
                         <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" 
-                            onChange={(e) => setAnswer(e.target.value)}/>
+                            onChange={(e) => setAnswer(e.target.value)} style={{borderColor:"#7D57C2"}}/>
                         <br/>
                         Thank you so much for volunteering to...., we really appreciate your support. 
                         Food banks rely on your attendance, so please do not sign up if you are not able to attend.
-                    </LowerSection>
+                    </DivPadding>
                     </div>
-                    <SubmitButton>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                        <SubmitText>Powered By Volunteria.</SubmitText>
-                </SubmitButton>
+                    <SubmitSection>
+                    <button type="button" className="btn btn-primary">Submit</button>
+                        <DivPadding>Powered By Volunteria.</DivPadding>
+                </SubmitSection>
                 </form>
                 </ContactContainer>
         </Container>        
