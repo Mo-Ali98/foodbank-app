3a754518ded5cdd1de1681de6d6b5b0372ff9e62
Abdiqani Haji Abdi <abdiqani.hajiabdi@smartsheet.com>
2022-06-19 12:52:57 +0100

    remove old volunteerin.tsx

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -5,7 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
- 
+  apiKey: "AIzaSyAYELTUmPug2_hTs7f_rfVhR4TVlLcyKrs",
+  authDomain: "food-bank-6e406.firebaseapp.com",
+  projectId: "food-bank-6e406",
+  storageBucket: "food-bank-6e406.appspot.com",
+  messagingSenderId: "608759665943",
+  appId: "1:608759665943:web:ab3d8bb57c152e504f5352",
 };
 
 // Initialize Firebase
