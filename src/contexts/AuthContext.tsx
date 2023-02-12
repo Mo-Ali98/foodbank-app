ea18e50a3e90af10275d93ba503d88689785e281
Mohamed Ahmed <41699093+Mo-Ali98@users.noreply.github.com>
2023-02-12 18:47:25 +0000

    ref: dashboard rework (#18)
    * ref: dashboard rework
    
    - move create an event and get user data to auth provider
    
    * Update firebase.json

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,53 +1,79 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, ReactNode } from "react";
 import { User } from "firebase/auth";
 import { auth, db } from "../firebase/firebase";
-import { doc, setDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  DocumentData,
+  getDoc,
+  getDocs,
+  setDoc,
+} from "firebase/firestore";
 import { FirebaseError } from "@firebase/util";
 import { IOrganisation } from "../models/organisation";
 import { notificationError } from "../components/notifications";
 import { useNavigate } from "react-router-dom";
+import { IEvent } from "../models/Event";
 
 interface ContextProps {
   user?: User;
   setUser: (user: User) => void;
   authenticated: boolean;
-  loadingAuthState: boolean;
+  loading: boolean;
   signUp: (email: string, password: string, OrganisationName: string) => void;
   logIn: (email: string, password: string) => void;
   logOut: () => void;
+  orgUserData: DocumentData;
+  eventsData: DocumentData;
+  volunteerData: DocumentData;
+  createNewEvent: (event: IEvent) => void;
 }
 
 const AuthContext = React.createContext<ContextProps>({
   user: undefined,
   setUser: () => ({} as Promise<void>),
   authenticated: false,
-  loadingAuthState: false,
+  loading: false,
   logIn: () => ({} as Promise<void>),
   signUp: () => ({} as Promise<void>),
   logOut: () => ({} as Promise<void>),
+  orgUserData: [],
+  eventsData: [],
+  volunteerData: [],
+  createNewEvent: () => ({} as Promise<void>),
 });
 
-export const AuthProvider = ({ children }: any) => {
-  const navigate = useNavigate();
+interface AuthContextProps {
+  children: ReactNode;
+}
 
+export const AuthProvider: React.FC<AuthContextProps> = ({ children }) => {
+  const navigate = useNavigate();
   const [user, setUser] = useState<User | undefined>(() =>
     JSON.parse(localStorage.getItem("user") || "{}")
   );
-  const [loadingAuthState, setLoadingAuthState] = useState(true);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [orgUserData, setOrgUserData] = useState<DocumentData>([]);
+  const [eventsData, setEventData] = useState<DocumentData>([]);
+  const [volunteerData, setVolunteerData] = useState<DocumentData>([]);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user: User | any) => {
+      setLoading(true);
       if (user) {
         setUser(user);
         localStorage.setItem("user", JSON.stringify(user));
-        setLoadingAuthState(false);
+        getData();
       } else {
         localStorage.removeItem("authUser");
         setUser(undefined);
       }
+      setLoading(false);
     });
 
     return unsubscribe;
+    // eslint-disable-next-line
   }, []);
 
   const logOut = () => {
@@ -93,7 +119,65 @@ export const AuthProvider = ({ children }: any) => {
       console.error(error);
       if (error instanceof FirebaseError) {
         notificationError(error.message);
-        console.error(error.code, error.cause, error.message, "here");
+        console.error(error.code, error.cause, error.message);
+      }
+    }
+  };
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+
+      if (user) {
+        //Fetch user/ org data
+        const orgRef = doc(db, "organisation", user.uid);
+        const orgSnap = await getDoc(orgRef);
+        const orgData = orgSnap.data();
+        setOrgUserData(orgData ?? []);
+
+        //fetch events
+        const eventsRef = collection(db, `events`);
+        const eventsQuerySnap = await getDocs(eventsRef);
+        setEventData(
+          eventsQuerySnap.docs.filter((doc) => {
+            const data = doc.data();
+            return data.OrgId === user.uid;
+          })
+        );
+
+        //Fetch volunteers
+        const VolunteersRef = collection(db, `Volunteer`);
+        const volunteersQuerySnap = await getDocs(VolunteersRef);
+        setVolunteerData(
+          volunteersQuerySnap.docs.filter((doc) => {
+            const data = doc.data();
+            return data.orgID === user.uid;
+          })
+        );
+      }
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof FirebaseError) {
+        notificationError(error.message);
+        console.error(error.code, error.cause, error.message);
+      }
+    }
+  };
+
+  const createNewEvent = async (newEvent: IEvent) => {
+    try {
+      setLoading(true);
+      if (user) {
+        await addDoc(collection(db, "events"), newEvent);
+      }
+      getData();
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof FirebaseError) {
+        notificationError(error.message);
+        console.error(error.code, error.cause, error.message);
       }
     }
   };
@@ -103,11 +187,15 @@ export const AuthProvider = ({ children }: any) => {
       value={{
         user,
         setUser,
-        authenticated: user !== null,
-        loadingAuthState,
+        authenticated: Boolean(user !== null),
+        loading,
         logOut,
         signUp,
         logIn,
+        eventsData,
+        orgUserData,
+        volunteerData,
+        createNewEvent,
       }}
     >
       {children}
